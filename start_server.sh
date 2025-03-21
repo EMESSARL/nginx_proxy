8452b38f779ccc2d162922d4917539dbe2ee86b3
Abousidikou ODJO <odjosidik0@gmail.com>
2025-03-21 19:58:11 +0100

    server

--- start_server.sh
+++ start_server.sh
@@ -10,10 +10,14 @@ if ! command -v docker-compose &> /dev/null; then
     echo "Docker Compose n'est pas installé. Veuillez l'installer pour continuer."
     exit 1
 fi
-
+cd ./server
 # Démarre le serveur avec Docker Compose en mode détaché
 echo "Démarrage du serveur Flask en mode détaché..."
+
+
+
 docker-compose up -d
 
+cd ~
 # Optionnel : Affiche les logs du conteneur
 echo "Pour voir les logs, exécutez : docker-compose logs -f"
